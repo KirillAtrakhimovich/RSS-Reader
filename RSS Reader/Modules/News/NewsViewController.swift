209805c2a3258edbb20d54f5b28927a9d45074ec
Kirill <kirill.atrakhimovich@gmail.com>
2023-02-02 21:12:24 +0300

    refactoring

--- RSS Reader/Modules/News/NewsViewController.swift
+++ RSS Reader/Modules/News/NewsViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 final class NewsViewController: NiblessViewController {
     
-    private var newsModel: Article?
+    private var newsModel = [Article]()
     let networkManager: NetworkManager
     let newsView = NewsView()
     
@@ -30,14 +30,21 @@ final class NewsViewController: NiblessViewController {
             guard let self = self else { return }
             switch result {
             case .success(let newsItem):
-                self.newsModel = newsItem.articles
+                self.newsModel = 
             case .failure(let error):
                print(error)
             }
         }
-        
     }
     
-    
+    private func convertNews(_ newsInfo: Articles) -> [Article] {
+        var newsItems = [Article]()
+        for item in newsInfo.articles {
+//            let name = namesIds.filter { $0.1 == newsInfo.appid }.first.map { $0.0 } ?? "1"
+            let model = Article(author: item.author, title: item.title, description: item.description, url: item.url, urlToImage: item.urlToImage, publishedAt: item.publishedAt, content: item.content)
+            newsItems.append(model)
+        }
+        return newsItems
+    }
     
 }
